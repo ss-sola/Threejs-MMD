82c8b326553644678ebf5436f694abe5ec8410b8
sola <2451477516@qq.com>
2023-03-18 16:12:32 +0800

    打包模型

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  assetsInclude: ['**/*.*'],
+  plugins: [vue(),tsconfigPaths()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
